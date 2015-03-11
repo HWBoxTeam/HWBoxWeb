962b333193310840476fb76b663055aec31dec42
alierdogan7 <erdoganaliburak@gmail.com>
2015-03-11 20:09:10 +0200

    app.js bug solved

--- js/app.js
+++ js/app.js
@@ -27,136 +27,178 @@
 	  		}
 	  	};
 	});
-
-	app.controller("CourseController", function(){
-		this.crss = crsObjects;
-	});
 	
-	app.controller("TabController", function(){
-		this.tab = 0;
-		this.setTab = function(tabIndex){
-			this.tab = tabIndex;
+	app.controller("PageController", function(){
+		this.page = 0;
+		this.setPage = function(pageIndex){
+			this.page = pageIndex;
+			console.log('setPage with' + pageIndex);
 		};
-		this.isSet = function(tabIndex){
-			return this.tab === tabIndex;
+		this.isSet = function(pageIndex){
+			return this.page === pageIndex;
+			console.log('isSet with' + pageIndex);
 		};
 	});
 
-	
-
-	app.controller("ViewHWController", function ($scope, $rootScope, dataService) {
-		
-		$scope.dataService = dataService;
-		//$scope.connectViewEdit = connectViewEdit;
-
-		/*Homework.getById("DDMspc3UjI", function(result){
-			Homeworks.data.push(result);
-			console.log(result + " : result from getById");
-		}.bind(this));*/
-		
-
-		this.sendHWToEdit = function(obj) {
-			console.log("set called with obj: " + JSON.stringify(obj));
-			//connectViewEdit.set(obj);
-			$rootScope.$broadcast('change.the.currentHW', obj );
-		};
-
-		this.deleteHW = function(obj){
-			var del = confirm("Are you sure you want to delete homework '" + obj.getName() + "'?");
-			var that = this;
-
-			if (del == true) {
-			   obj.delete(function(bool, msg){
-			   		console.log("delete's callback function called");
-			   		alert(msg);
-			   		$scope.dataService.load(function(){
-			   			$scope.$apply(); //after loading refresht the controller's view
-			   			console.log("dataService.load callback fcn called");
-			   		});
-			   });
-			}
-		}
-	});
-	
-	app.controller("addHWController", function($scope, dataService){
-		$scope.dataService = dataService;
-
-		this.newHW = {};
-		this.newHW.hwDueDate = Date.now();
-		var isSuccessful = false;
-	    var isSubmitted = false;
-	    var msg;
-		
-		this.addHW = function(){
-			
-			var hw = new Homework();
-		
-			hw.set("hwDone", this.newHW.hwDone);
-			hw.set("hwDescription", this.newHW.hwDescription);
-			hw.set("hwName", this.newHW.hwName);
-			hw.set("hwDueDate", new Date(this.newHW.hwDueDate));
-			hw.set("hwDone", false);
-
-			hw.insert(function(bool, msg){
-				this.isSuccessful = bool;
-                this.isSubmitted = true;
-                this.msg = msg;
-                console.log("insert callback called, isSubmitted: " + this.isSubmitted + " isSuccessful: " + this.isSuccessful);
-				this.newHW = {}; //reset the form
-			}.bind(this));
-			
-			$scope.dataService.load(function(){
-			   	$scope.$apply(); //after loading refresht the controller's view
-			});
-		};
-	});
-
-	
-	
-	app.controller("editHWController", function($scope, dataService){
-		
-		$scope.dataService = dataService;
-		$scope.HW = {};
-
-		var parseHWObj;
-		var isSuccessful = false;
-	    var isSubmitted = false;
-	    var msg;
-		
-		$scope.$on('change.the.currentHW', function(event, value){
-			console.log("broadcast $on function called");
-			parseHWObj = value;
-			$scope.HW.hwName = parseHWObj.getName(); 
-			$scope.HW.hwDescription = parseHWObj.getDescription();
-			$scope.HW.hwDueDate = parseHWObj.getDueDate();
-			$scope.HW.hwDone = parseHWObj.isDone(); 
-		});
-		
-		this.editHW = function(){
-
-			parseHWObj.set("hwName", $scope.HW.hwName);
-			parseHWObj.set("hwDescription", $scope.HW.hwDescription);
-			parseHWObj.set("hwDueDate", $scope.HW.hwDueDate);
-			parseHWObj.set("hwDone", $scope.HW.hwDone);
-
-			parseHWObj.update(function(bool, msg){
-				this.isSuccessful = bool;
-				this.isSubmitted = true;
-				this.msg = msg;
-				$scope.$apply();
-			}.bind(this));
-
-			$scope.dataService.load(function(){
-				$scope.HW = {};
-	   			$scope.$apply(); //after loading refresht the controller's view
-	   		});
-
-		};
-	});
 	
 	app.controller("denemeController", function(){
 		
 	});
+
+	//////////////////////////////////////////////
+	///////////////// DIRECTIVES /////////////////
+	//////////////////////////////////////////////
+
+	app.directive('addHomework', function(){
+		return {
+			restrict: 'E',
+			templateUrl: 'directives/add-homework.html',
+			controller: function($scope, dataService){
+				$scope.dataService = dataService;
+
+				this.newHW = {};
+				this.newHW.hwDueDate = Date.now();
+				var isSuccessful = false;
+			    var isSubmitted = false;
+			    var msg;
+				
+				this.addHW = function(){
+					
+					var hw = new Homework();
+				
+					hw.set("hwDone", this.newHW.hwDone);
+					hw.set("hwDescription", this.newHW.hwDescription);
+					hw.set("hwName", this.newHW.hwName);
+					hw.set("hwDueDate", new Date(this.newHW.hwDueDate));
+					hw.set("hwDone", false);
+
+					hw.insert(function(bool, msg){
+						this.isSuccessful = bool;
+		                this.isSubmitted = true;
+		                this.msg = msg;
+		                console.log("insert callback called, isSubmitted: " + this.isSubmitted + " isSuccessful: " + this.isSuccessful);
+						this.newHW = {}; //reset the form
+					}.bind(this));
+					
+					$scope.dataService.load(function(){
+					   	$scope.$apply(); //after loading refresht the controller's view
+					});
+				};
+			},
+			controllerAs: 'addHWCtrl'
+		};
+	});
+
+	app.directive('editHomework', function(){
+		return {
+			restrict: 'E',
+			templateUrl: 'directives/edit-homework.html',
+			controller: function($scope, dataService){
+				$scope.dataService = dataService;
+				$scope.HW = {};
+
+				var parseHWObj;
+				var isSuccessful = false;
+			    var isSubmitted = false;
+			    var msg;
+				
+				$scope.$on('change.the.currentHW', function(event, value){
+					console.log("broadcast $on function called");
+					parseHWObj = value;
+					$scope.HW.hwName = parseHWObj.getName(); 
+					$scope.HW.hwDescription = parseHWObj.getDescription();
+					$scope.HW.hwDueDate = parseHWObj.getDueDate();
+					$scope.HW.hwDone = parseHWObj.isDone(); 
+				});
+				
+				this.editHW = function(){
+
+					parseHWObj.set("hwName", $scope.HW.hwName);
+					parseHWObj.set("hwDescription", $scope.HW.hwDescription);
+					parseHWObj.set("hwDueDate", $scope.HW.hwDueDate);
+					parseHWObj.set("hwDone", $scope.HW.hwDone);
+
+					parseHWObj.update(function(bool, msg){
+						this.isSuccessful = bool;
+						this.isSubmitted = true;
+						this.msg = msg;
+						$scope.$apply();
+					}.bind(this));
+
+					$scope.dataService.load(function(){
+						$scope.HW = {};
+			   			$scope.$apply(); //after loading refresht the controller's view
+			   		});
+
+				};
+			},
+			controllerAs: 'editHWCtrl'
+		};
+	});
+
+	app.directive('viewHomeworks', function(){
+		return {
+			restrict: 'E',
+			templateUrl: 'directives/view-homeworks.html',
+			controller: function ($scope, $rootScope, dataService) {
+				$scope.dataService = dataService;
+				//$scope.connectViewEdit = connectViewEdit;
+
+				/*Homework.getById("DDMspc3UjI", function(result){
+					Homeworks.data.push(result);
+					console.log(result + " : result from getById");
+				}.bind(this));*/
+				
+
+				this.sendHWToEdit = function(obj) {
+					console.log("set called with obj: " + JSON.stringify(obj));
+					//connectViewEdit.set(obj);
+					$rootScope.$broadcast('change.the.currentHW', obj );
+				};
+
+				this.deleteHW = function(obj){
+					var del = confirm("Are you sure you want to delete homework '" + obj.getName() + "'?");
+					var that = this;
+
+					if (del == true) {
+					   obj.delete(function(bool, msg){
+					   		console.log("delete's callback function called");
+					   		alert(msg);
+					   		$scope.dataService.load(function(){
+					   			$scope.$apply(); //after loading refresht the controller's view
+					   			console.log("dataService.load callback fcn called");
+					   		});
+					   });
+					}
+				};
+			},
+			controllerAs: 'viewHwCtrl'
+		};
+	});
+
+	app.directive('courses', function(){
+		return {
+			restrict: 'E',
+			templateUrl: 'directives/courses.html',
+			controller: function(){
+				this.crss = crsObjects;
+			},
+			controllerAs: 'crsCtrl'
+		};		
+	});
+
+	app.directive('dashboard', function(){
+		return {
+			restrict: 'E',
+			templateUrl: 'directives/dashboard.html',
+			controller: function(){
+
+			},
+			controllerAs: 'dashboardCtrl'
+		};
+	});
+
+
 	
 	/*var hwObjects = [
 		//DUMMY DATAS
